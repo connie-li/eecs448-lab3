fdba34f9a0e6649d7b6ddf3359a06234d724e417
Connie Li <constanceli@ku.edu>
2019-09-23 14:01:47 -0500

    test (ex1): bug fixes

--- ex1-validate/validate.js
+++ ex1-validate/validate.js
@@ -7,16 +7,16 @@ function checkEqual(id1, id2) {
     let pw1 = document.getElementById(id1);
     let pw2 = document.getElementById(id2);
     if(pw1 == pw2) {
-        document.getElementById("msg").innerHTML = "Password validated!";
+        document.getElementById("msg1").innerHTML = "Password validated!";
     }
     else {
-        document.getElementById("msg").innerHTML = "Passwords do not match."
+        document.getElementById("msg1").innerHTML = "Passwords do not match."
     }
 }
 
 function checkLength(id1, id2) {
-    if((pw1.length() >= 8) || (pw3.length() >=8))
+    if((pw1.length <= 8) || (pw2.length <=8))
     {
-        
+        document.getElementById("msg2").innerHTML = "Password must be at least 8 characters long.";
     }
 }
