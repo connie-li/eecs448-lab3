8dac6d8cba0676be07352f974c7b4b83593cb6d2
Connie Li <constanceli@ku.edu>
2019-09-23 13:55:40 -0500

    feat (ex1): add checkLength function and rename/streamline checkEqual

--- ex1-validate/validate.js
+++ ex1-validate/validate.js
@@ -1,8 +1,17 @@
-function validate(id1, id2) {
-    if(document.getElementById(id1) == document.getElementById(id2)) {
+function checkEqual(id1, id2) {
+    let pw1 = document.getElementById(id1);
+    let pw2 = document.getElementById(id2);
+    if(pw1 == pw2) {
         document.getElementById("msg").innerHTML = "Password validated!";
     }
     else {
         document.getElementById("msg").innerHTML = "Passwords do not match."
+    }
+}
+
+function checkLength(id1, id2) {
+    if((pw1.length() >= 8) || (pw3.length() >=8))
+    {
+        
     }
 }
