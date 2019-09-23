0c679d733d5f52755e9ef7cd98a10517a63777a1
Connie Li <constanceli@ku.edu>
2019-09-23 14:50:44 -0500

    refactor(ex1): move getting input field values into validate

--- ex1-validate/validate.js
+++ ex1-validate/validate.js
@@ -1,13 +1,11 @@
 function validate(id1, id2) {
-    if(checkEqual(id1, id2) && checkLength(id1, id2)) {
+    let input1 = document.getElementById(id1).value;
+    let input2 = document.getElementById(id2).value;
+    if(checkEqual(input1, input2) && checkLength(input1, input2)) {
         document.getElementById("valmsg").innerHTML = "Password validated!";
     }
 }
 
-function checkEqual(id1, id2) {
-    let pw1 = document.getElementById(id1);
-    let pw2 = document.getElementById(id2);
-    if(pw1 == pw2) {
         return true;
     }
     else {
