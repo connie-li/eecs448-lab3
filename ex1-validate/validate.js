7fa4446e9220b6604c5afd13935c31dc13bc74a7
Connie Li <constanceli@ku.edu>
2019-09-23 13:47:32 -0500

    feat (ex1): link js to html add meta tag, edit formatting, change an HTML id

--- ex1-validate/validate.js
+++ ex1-validate/validate.js
@@ -1,8 +1,8 @@
 function validate(id1, id2) {
     if(document.getElementById(id1) == document.getElementById(id2)) {
-        document.getElementById("msg1").innerHTML = "Password validated!";
+        document.getElementById("msg").innerHTML = "Password validated!";
     }
     else {
-        document.getElementById("msg1").innerHTML = "Passwords do not match."
+        document.getElementById("msg").innerHTML = "Passwords do not match."
     }
 }
