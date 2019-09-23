8852eef86623020170ff0b5b6ea5aa4cce42faee
Connie Li <constanceli@ku.edu>
2019-09-23 14:04:07 -0500

    refactor: make checker functions return booleans

--- ex1-validate/validate.js
+++ ex1-validate/validate.js
@@ -1,16 +1,18 @@
 function validate(id1, id2) {
-    checkEqual(id1, id2);
-    checkLength(id1, id2);
+    if(checkEqual(id1, id2) && checkLength(id1, id2)) {
+        document.getElementById("valmsg").innerHTML = "Password validated!";
+    }
 }
 
 function checkEqual(id1, id2) {
     let pw1 = document.getElementById(id1);
     let pw2 = document.getElementById(id2);
     if(pw1 == pw2) {
-        document.getElementById("msg1").innerHTML = "Password validated!";
+        return true;
     }
     else {
         document.getElementById("msg1").innerHTML = "Passwords do not match."
+        return false;
     }
 }
 
@@ -18,5 +20,6 @@ function checkLength(id1, id2) {
     if((pw1.length <= 8) || (pw2.length <=8))
     {
         document.getElementById("msg2").innerHTML = "Password must be at least 8 characters long.";
+        return true;
     }
 }
