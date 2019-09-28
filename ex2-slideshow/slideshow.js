83778af5357a4d046e8955774d435bd45c03bbf2
Connie <constanceli@ku.edu>
2019-09-28 14:41:27 -0500

    feat(ex2): implement buttonClick function to handle both buttons

--- ex2-slideshow/slideshow.js
+++ ex2-slideshow/slideshow.js
@@ -1,4 +1,5 @@
 let index = 0;
+let numSlides = 5;
 showSlide(0);
 
 function showSlide(slideIndex) {
@@ -12,3 +13,28 @@ function hideSlide(slideIndex) {
 /**
  * hide current img, change index, show next/prev img
  */
+function buttonClick(direction) {
+	// show next image
+	if (direction > 0) {
+		hideSlide(index);
+		index++;
+		if(index >= numSlides)
+		{
+			index = 0;
+		}
+		showSlide(index);
+	}
+	// show previous image
+	else if(direction < 0) {
+		hideSlide(index);
+		index--;
+		if(index < 0)
+		{
+			index = numSlides - 1;
+		}
+		showSlide(index);
+	}
+	else {
+		console.log("Congratulations! You have somehow broken the buttons.");
+	}
+}
