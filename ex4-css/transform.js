ebdd0c722486c2b9b44d64487b36129432a9e12a
Connie <constanceli@ku.edu>
2019-09-30 06:25:53 -0500

    feat(ex4): implement transform function
    known issue: border color & width values can be altered, but no border appears.

--- ex4-css/transform.js
+++ ex4-css/transform.js
@@ -1,3 +1,14 @@
-function transform(borderR, borderG, borderB, bgR, bgG, bgB) {
+let contents = document.getElementById("contents");
 
+function transform() {
+    let borderR = document.getElementById("borderR").value;
+    let borderG = document.getElementById("borderG").value;
+    let borderB = document.getElementById("borderB").value;
+    let borderW = document.getElementById("borderW").value;
+    let bgR = document.getElementById("bgR").value;
+    let bgG = document.getElementById("bgG").value;
+    let bgB = document.getElementById("bgB").value;
+    contents.style.borderColor = "rgb(" + borderR + "," + borderG + "," + borderB + ")";
+    contents.style.borderWidth = borderW + "px";
+    contents.style.backgroundColor = "rgb(" + bgR + "," + bgG + "," + bgB + ")";
 }
